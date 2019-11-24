934da9166bfdff054f08c798f799945e833651e5
Martin Morales <isthemartin@gmail.com>
2019-11-23 18:16:57 -0600

    Added CRUD in UI
    CRUD was added in a simple way throught MVC.
    UI and WebAPI works succesfully.

---
+++ HandCloudExam/UsedCarsUI/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UsedCarsUI.Models;
+
+namespace UsedCarsUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        Helper _helper = new Helper();
+
+        public async Task<IActionResult> Index()
+        {
+            List<Car> carList = new List<Car>();
+            HttpClient client = _helper.Initial();
+            HttpResponseMessage httpResponse = await client.GetAsync("hc/UsedCar/GetUsedCars");
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var results = httpResponse.Content.ReadAsStringAsync().Result;
+                carList = JsonConvert.DeserializeObject<List<Car>>(results);
+            }
+            return View(carList);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            Car car = new Car();
+            HttpClient client = _helper.Initial();
+            HttpResponseMessage httpResponse = await client.GetAsync($"hc/UsedCar/GetUsedCar/?Id={id}");
+            if(httpResponse.IsSuccessStatusCode)
+            {
+                var results = httpResponse.Content.ReadAsStringAsync().Result;
+                car = JsonConvert.DeserializeObject<Car>(results);
+            }
+            return View(car);
+        }
+
+        public IActionResult Add(Car car)
+        {
+            HttpClient client = _helper.Initial();
+            var post = client.PostAsJsonAsync<Car>("hc/UsedCar/AddUsedCar", car);
+            if (post.Result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            Car car = new Car();
+            HttpClient client = _helper.Initial();
+            HttpResponseMessage httpResponse = await client.DeleteAsync($"hc/UsedCar/DeleteUsedCar?Id={id}");
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            Car car = new Car();
+            HttpClient client = _helper.Initial();
+            HttpResponseMessage httpResponse = await client.GetAsync($"hc/UsedCar/GetUsedCar/?Id={id}");
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var results = httpResponse.Content.ReadAsStringAsync().Result;
+                car = JsonConvert.DeserializeObject<Car>(results);
+            }
+            if(car != null)
+                return View(car);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(Car car)
+        {
+            HttpClient client = _helper.Initial();
+            var put = client.PutAsJsonAsync<Car>("hc/UsedCar/UpdateUsedCar", car);
+            if (put.Result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HandCloudExam/UsedCarsUI/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UsedCarsUI
+{
+    public class Helper
+    {
+        public HttpClient Initial()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44389/");
+            return client;
+        }
+
+    }
+}
